71bd72fed8e77f563e7546cb3f822b8ed35f86d6
Eugen1344 <eugen1344@gmail.com>
2023-06-23 20:25:43 +0300

    + Playmode Starting Scene

---
+++ PlaymodeStartingSceneSelector/Editor/PlaymodeStartingSceneSelector.cs
@@ -0,0 +1,37 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class PlaymodeStartingSceneSelector : EditorWindow
+{
+    [MenuItem("Tools/Playmode Starting Scene")]
+    static void Open()
+    {
+        GetWindow<PlaymodeStartingSceneSelector>("Starting Scene");
+    }
+
+    private void OnEnable()
+    {
+        PlaymodeStartingSceneSetter.SetPlayModeStartScene(PlaymodeStartingSceneSetter.LoadStartingSceneFromEditorPrefs());
+    }
+
+    private void OnGUI()
+    {
+        EditorGUIUtility.labelWidth = 80;
+        EditorGUI.BeginChangeCheck();
+        EditorGUILayout.HelpBox("This scene will always be loaded when you press play. " +
+                                "After you press stop you will return to scene you'll been working on.", MessageType.Info);
+        SceneAsset selectedScene = (SceneAsset) EditorGUILayout.ObjectField(new GUIContent("Start Scene"),
+            EditorSceneManager.playModeStartScene, typeof(SceneAsset), false);
+        if (EditorGUI.EndChangeCheck())
+        {
+            SetAndSaveStartingScene(selectedScene);
+        }
+    }
+
+    private static void SetAndSaveStartingScene(SceneAsset selectedScene)
+    {
+        PlaymodeStartingSceneSetter.SetPlayModeStartScene(selectedScene);
+        PlaymodeStartingSceneSetter.SaveStartingSceneFromEditorPrefs(selectedScene);
+    }
+}
---
+++ PlaymodeStartingSceneSelector/Editor/PlaymodeStartingSceneSetter.cs
@@ -0,0 +1,55 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class PlaymodeStartingSceneSetter
+{
+    private const string StartingSceneKey = "playmode_starting_scene";
+
+    static PlaymodeStartingSceneSetter()
+    {
+        SceneAsset selectedScene = LoadStartingSceneFromEditorPrefs();
+        if (!selectedScene)
+        {
+            return;
+        }
+
+        SetPlayModeStartScene(selectedScene);
+    }
+
+    public static void SaveStartingSceneFromEditorPrefs(SceneAsset selectedScene)
+    {
+        if (selectedScene == null)
+        {
+            EditorPrefs.SetString(StartingSceneKey + "_" + Application.productName, "");
+            return;
+        }
+
+        string scenePath = AssetDatabase.GetAssetPath(selectedScene);
+        string sceneGuid = AssetDatabase.AssetPathToGUID(scenePath);
+        EditorPrefs.SetString(StartingSceneKey + "_" + Application.productName, sceneGuid);
+    }
+
+    public static SceneAsset LoadStartingSceneFromEditorPrefs()
+    {
+        string sceneGuid = EditorPrefs.GetString(StartingSceneKey + "_" + Application.productName);
+        if (string.IsNullOrEmpty(sceneGuid))
+        {
+            return null;
+        }
+
+        string scenePath = AssetDatabase.GUIDToAssetPath(sceneGuid);
+        if (string.IsNullOrEmpty(scenePath))
+        {
+            return null;
+        }
+
+        return AssetDatabase.LoadAssetAtPath<SceneAsset>(scenePath);
+    }
+
+    public static void SetPlayModeStartScene(SceneAsset selectedScene)
+    {
+        EditorSceneManager.playModeStartScene = selectedScene;
+    }
+}
