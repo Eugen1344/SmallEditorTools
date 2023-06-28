46b43ec6548e778b1ced388ab88dbd33b614e964
Eugen1344 <eugen1344@gmail.com>
2023-06-29 02:59:22 +0300

    refactored scriptable object creator

---
+++ ScriptableObjectCreator/Editor/ScriptableObjectCreator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace SmallEditorTools.ScriptableObjectCreator
+{
+    public class ScriptableObjectCreator
+    {
+        public delegate ScriptableObject ScriptableObjectFactoryMethod(Type type);
+
+        public void CreateObject(string typeName, string objectName, ScriptableObjectFactoryMethod factoryMethod)
+        {
+            if (!ValidateName(objectName))
+                return;
+
+            if (string.IsNullOrWhiteSpace(typeName))
+                return;
+
+            Type objectType = GetObjectType(typeName);
+
+            if (objectType == null)
+            {
+                Debug.LogError($"Type \"{typeName}\" was not found!");
+                return;
+            }
+
+            string path = GetPath();
+            string assetPath = Path.Combine(path, $"{objectName}.asset");
+            string uniqueAssetPath = AssetDatabase.GenerateUniqueAssetPath(assetPath);
+
+            ScriptableObject newObject = factoryMethod(objectType);
+            AssetDatabase.CreateAsset(newObject, uniqueAssetPath);
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+            EditorUtility.FocusProjectWindow();
+            Selection.activeObject = newObject;
+        }
+
+        private bool ValidateName(string objectName)
+        {
+            if (string.IsNullOrWhiteSpace(objectName))
+            {
+                Debug.LogError($"Name: \"{objectName}\" is invalid");
+                return false;
+            }
+
+            return true;
+        }
+
+        private Type GetObjectType(string name)
+        {
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (Type type in assembly.GetTypes().Where(type => type.IsSubclassOf(typeof(ScriptableObject)) && string.Equals(type.Name, name, StringComparison.OrdinalIgnoreCase)))
+                {
+                    return type;
+                }
+            }
+
+            return null;
+        }
+
+        private string GetPath()
+        {
+            string selectedObjectPath = AssetDatabase.GetAssetPath(Selection.activeObject.GetInstanceID());
+
+            if (string.IsNullOrWhiteSpace(selectedObjectPath))
+                return "Assets";
+
+            if (Directory.Exists(selectedObjectPath))
+                return selectedObjectPath;
+
+            return Path.GetDirectoryName(selectedObjectPath);
+        }
+    }
+}
--- ScriptableObjectCreator/Editor/ScriptableObjectCreatorWindow.cs
+++ ScriptableObjectCreator/Editor/ScriptableObjectCreatorWindow.cs
@@ -1,24 +1,27 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using UnityEditor;
+﻿using UnityEditor;
 using UnityEngine;
 
 namespace SmallEditorTools.ScriptableObjectCreator
 {
     public class ScriptableObjectCreatorWindow : EditorWindow
     {
+        private ScriptableObjectCreator _creator;
+
         private string _typeName;
         private string _objectName = "New Object";
 
         [MenuItem("Tools/Scriptable Object Creator")]
-        static void Open()
+        private static void Open()
         {
             GetWindow<ScriptableObjectCreatorWindow>("Scriptable Object Creator");
         }
 
-        void OnGUI()
+        private void OnEnable()
+        {
+            _creator = new ScriptableObjectCreator();
+        }
+
+        private void OnGUI()
         {
             EditorGUIUtility.labelWidth = 200;
             _typeName = EditorGUILayout.TextField(new GUIContent("Scriptable object TYPE name"), _typeName);
@@ -26,74 +29,8 @@ namespace SmallEditorTools.ScriptableObjectCreator
 
             if (GUILayout.Button(new GUIContent("Create")))
             {
-                CreateObject();
+                _creator.CreateObject(_typeName, _objectName, CreateInstance);
             }
-        }
-
-        public void CreateObject()
-        {
-            if (!CheckName(_objectName))
-                return;
-
-            if (string.IsNullOrWhiteSpace(_typeName))
-                return;
-
-            Type objectType = GetObjectType(_typeName);
-
-            if (objectType == null)
-            {
-                Debug.LogError($"Type \"{_typeName}\" was not found!");
-                return;
-            }
-
-            string path = GetPath();
-            string assetPath = Path.Combine(path, $"{_objectName}.asset");
-            string uniqueAssetPath = AssetDatabase.GenerateUniqueAssetPath(assetPath);
-
-            ScriptableObject newObject = CreateInstance(objectType);
-            AssetDatabase.CreateAsset(newObject, uniqueAssetPath);
-
-            AssetDatabase.SaveAssets();
-            AssetDatabase.Refresh();
-            EditorUtility.FocusProjectWindow();
-            Selection.activeObject = newObject;
-        }
-
-        private bool CheckName(string objectName)
-        {
-            if (string.IsNullOrWhiteSpace(objectName))
-            {
-                Debug.LogError($"Name: \"{objectName}\" is invalid");
-                return false;
-            }
-
-            return true;
-        }
-
-        private Type GetObjectType(string name)
-        {
-            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
-            {
-                foreach (Type type in assembly.GetTypes().Where(type => type.IsSubclassOf(typeof(ScriptableObject)) && string.Equals(type.Name, name, StringComparison.OrdinalIgnoreCase)))
-                {
-                    return type;
-                }
-            }
-
-            return null;
-        }
-
-        private string GetPath()
-        {
-            string selectedObjectPath = AssetDatabase.GetAssetPath(Selection.activeObject.GetInstanceID());
-
-            if (string.IsNullOrWhiteSpace(selectedObjectPath))
-                return "Assets";
-
-            if (Directory.Exists(selectedObjectPath))
-                return selectedObjectPath;
-
-            return Path.GetDirectoryName(selectedObjectPath);
         }
     }
 }
