6417eeb49123f5b91cc32a746efc02a8358e70c8
Eugen1344 <eugen1344@gmail.com>
2023-08-28 03:20:54 +0300

    + PlayerPrefsClearer

---
+++ PlayerPrefsClearer/PlayerPrefsClearer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Submodules.SmallEditorTools.PlayerPrefsClearer
+{
+    public static class PlayerPrefsClearer
+    {
+        private const string Title = "Delete all player preferences";
+        private const string Message = "Are you sure you want to delete all player preferences?\nThis action cannot be undone.";
+
+        [MenuItem("Tools/Clear All PlayerPrefs")]
+        private static void ClearAllEditorPrefs()
+        {
+            if (EditorUtility.DisplayDialog(Title, Message, "Yes", "No"))
+                PlayerPrefs.DeleteAll();
+        }
+    }
+}
